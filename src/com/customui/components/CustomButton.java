0371a0d6a2fb64b91343418d69a96a81c478e786
Aiko <aaikoalvarez@gmail.com>
2026-09-19 19:38:21 -0600

    Initial commit: Custom Swing components

---
+++ src/com/customui/components/CustomButton.java
@@ -0,0 +1,51 @@
+package com.customui.components;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.*;
+
+public class CustomButton extends JButton {
+
+    private Color normalColor = new Color (79,70,229);
+    private Color hoverColor = new Color (99, 102, 241);
+    private Color pressColor = new Color (67,56,202);
+    private Color currentColor = normalColor;
+    private int cornerRadius = 15;
+
+    public CustomButton (String text){
+        super (text);
+        setContentAreaFilled(false);
+        setFocusPainted(false);
+        setBorderPainted(false);
+        setForeground(Color.white);
+        setFont (new Font ("SansSerif", Font.BOLD,14));
+        setCursor (new Cursor (Cursor.HAND_CURSOR));
+
+        addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseEntered (MouseEvent e){
+                currentColor = hoverColor;
+                repaint ();
+            }
+
+            @Override
+            public void mouseExited (MouseEvent e){
+                currentColor = normalColor;
+                repaint ();
+            }
+
+            @Override
+            public void mousePressed (MouseEvent e){
+                currentColor = pressColor;
+                repaint();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e){
+                currentColor = normalColor;
+                repaint ();
+            }
+        });
+    }
+}
